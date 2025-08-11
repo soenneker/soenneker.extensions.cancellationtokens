9243e34ef60f3efa82eee684411829ca107021f6
Jake Soenneker <jake@soenneker.com>
2025-08-11 12:28:56 -0500

    Initial commit

---
+++ test/Soenneker.Extensions.CancellationTokens.Tests/CancellationTokensExtensionTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Extensions.CancellationTokens.Tests;
+
+public sealed class CancellationTokensExtensionTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
