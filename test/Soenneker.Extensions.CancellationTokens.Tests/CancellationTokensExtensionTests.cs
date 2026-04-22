11d474384d2893dec3f403c2d54eb26b0a984675
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:49 -0500

    TUnit conversion

--- test/Soenneker.Extensions.CancellationTokens.Tests/CancellationTokensExtensionTests.cs
+++ test/Soenneker.Extensions.CancellationTokens.Tests/CancellationTokensExtensionTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Extensions.CancellationTokens.Tests;
 
 public sealed class CancellationTokensExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
